686a2f6f949d70c8d4f8ed5aacbcbcef13837fe9
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-22 08:58:42 -0600

    association added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :menus
+  has_many :tables
   has_many :menu_items, through: :menus
   has_many :line_items, through: :menu_items
   has_many :orders, through: :line_items
